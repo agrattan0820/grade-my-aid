129921450d197bde895199d2b46c4b4962293a6e
Alexander Grattan <alex@grattan.me>
2020-09-28 21:30:29 -0400

    Grabbing from json with async and fetch

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const data = require("./universityData");
-console.log(data);
+async function getData() {
+  let response = await fetch("./universityData.json");
+  let data = response.json();
+  return data;
+}
+
+getData()
+  .then((data) => console.log(data))
+  .catch((reason) => console.log(reason.message));
+
+const container = document.querySelector(".container");
