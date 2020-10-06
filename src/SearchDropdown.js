fa03207a900f3de05e5cef7faec5363fe4231788
Alexander Grattan <alex@grattan.me>
2020-10-06 15:20:17 -0400

    Clickable input and options

--- src/SearchDropdown.js
+++ src/SearchDropdown.js
@@ -3,30 +3,44 @@ import "./App.css";
 
 function SearchDropdown({ data }) {
   const [search, setSearch] = useState("");
+  const [display, setDisplay] = useState(false);
 
   const filteredData = data.filter((university) => {
     return university.institution.toLowerCase().includes(search.toLowerCase());
   });
 
-  const updateSearch = (e) => {
+  const typeSearch = (e) => {
     setSearch(e.target.value);
+  };
+
+  const clickSearch = (school) => {
+    setSearch(school);
+    setDisplay(false);
   };
 
   return (
     <div className="dropdown">
       <input
         className="search-bar"
+        placeholder="Search for a school"
         type="text"
+        onClick={() => setDisplay(!display)}
         value={search}
-        onChange={updateSearch}
+        onChange={typeSearch}
       />
-      <div className="dropdown-schools">
-        {filteredData.map((university, index) => (
-          <div className="option" key={index}>
-            <span>{university.institution}</span>
-          </div>
-        ))}
-      </div>
+      {display && (
+        <div className="dropdown-schools">
+          {filteredData.map((university, index) => (
+            <div
+              onClick={() => clickSearch(university.institution)}
+              className="option"
+              key={index}
+            >
+              <span>{university.institution}</span>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
