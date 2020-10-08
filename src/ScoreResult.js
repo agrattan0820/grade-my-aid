da6c54827f136a1308b17fbd5d259b784e763482
Alexander Grattan <alex@grattan.me>
2020-10-08 16:00:26 -0400

    Alignment of containers

--- src/ScoreResult.js
+++ src/ScoreResult.js
@@ -25,36 +25,60 @@ function ScoreResult() {
 
     const filteredSchool = filteredData[0];
 
-    if (aidValue >= 40000) {
+    if (aidValue >= 50000) {
       console.log("aid added");
-      pointCounter += 10;
+      pointCounter += 30;
+    } else if (aidValue >= 45000) {
+      pointCounter += 25;
+    } else if (aidValue >= 40000) {
+      pointCounter += 20;
+    } else if (aidValue >= 35000) {
+      pointCounter += 18;
+    } else if (aidValue >= 30000) {
+      pointCounter += 16;
     } else if (aidValue >= 25000) {
-      pointCounter += 7;
+      pointCounter += 14;
+    } else if (aidValue >= 20000) {
+      pointCounter += 12;
     } else if (aidValue >= 15000) {
-      pointCounter += 5;
+      pointCounter += 10;
+    } else if (aidValue >= 10000) {
+      pointCounter += 8;
     } else if (aidValue >= 5000) {
+      pointCounter += 6;
+    } else {
       pointCounter += 2;
     }
 
     if (filteredSchool.national_rank <= 5) {
-      pointCounter += 10;
+      pointCounter += 30;
       console.log("national added");
-    } else if (filteredSchool.national_rank <= 25) {
-      pointCounter += 7;
     } else if (filteredSchool.national_rank <= 50) {
-      pointCounter += 5;
+      pointCounter += 25;
     } else if (filteredSchool.national_rank <= 100) {
+      pointCounter += 20;
+    } else if (filteredSchool.national_rank <= 200) {
+      pointCounter += 15;
+    } else if (filteredSchool.national_rank <= 500) {
+      pointCounter += 5;
+    } else {
       pointCounter += 2;
     }
 
     if (filteredSchool.alumni_employment.to <= 5) {
-      pointCounter += 10;
+      pointCounter += 30;
       console.log("alumni added");
     } else if (filteredSchool.alumni_employment <= 25) {
-      pointCounter += 7;
+      pointCounter += 25;
     } else if (filteredSchool.alumni_employment <= 50) {
-      pointCounter += 5;
+      pointCounter += 20;
     } else if (filteredSchool.alumni_employment <= 100) {
+      pointCounter += 15;
+    } else if (filteredSchool.alumni_employment <= 200) {
+      pointCounter += 10;
+    } else if (filteredSchool.alumni_employment <= 500) {
+      pointCounter += 5;
+    } else {
       pointCounter += 2;
     }
 
@@ -66,14 +90,24 @@ function ScoreResult() {
       console.log(pointCounter);
     }
 
-    if (pointCounter >= 25) {
+    if (pointCounter >= 80) {
+      setScore("A+");
+    } else if (pointCounter >= 60) {
       setScore("A");
-    } else if (pointCounter >= 20) {
+    } else if (pointCounter >= 45) {
+      setScore("A-");
+    } else if (pointCounter >= 35) {
+      setScore("B+");
+    } else if (pointCounter >= 25) {
       setScore("B");
     } else if (pointCounter >= 15) {
-      setScore("C");
+      setScore("B-");
     } else if (pointCounter >= 10) {
-      setScore("D");
+      setScore("C+");
+    } else if (pointCounter >= 5) {
+      setScore("C");
+    } else {
+      setScore("C-");
     }
   };
 
